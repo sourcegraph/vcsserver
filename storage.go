d817cd5989c6cdfc6fc3dda38e0792552468e04a
Quinn Slack <qslack@qslack.com>
2014-02-04 15:58:39 -0800

    check more local storage locations when running offline

--- storage.go
+++ storage.go
@@ -2,12 +2,29 @@ package vcsserver
 
 import (
 	"github.com/sourcegraph/go-vcs"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 // StorageDir is the root directory underneath which repositories are stored.
 var StorageDir = "/tmp/vcsserver"
 
 func repoDir(vcs vcs.VCS, uri string) string {
-	return filepath.Join(StorageDir, vcs.ShortName(), uri)
+	preferred := filepath.Join(StorageDir, vcs.ShortName(), uri)
+
+	// if we're running offline, try harder to find a local copy
+	if Offline {
+		if alternate := strings.TrimSuffix(preferred, ".git"); isDir(alternate) {
+			return alternate
+		}
+	}
+
+	return preferred
+}
+
+// IsDir returns true if path is an existing directory, and false otherwise.
+func isDir(path string) bool {
+	fi, err := os.Stat(path)
+	return err == nil && fi.IsDir()
 }
