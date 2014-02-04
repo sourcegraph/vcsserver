e294ac86f0df3fee1d264c6970108acd380176b5
Quinn Slack <qslack@qslack.com>
2014-02-04 15:01:51 -0800

    FilePath

--- url.go
+++ url.go
@@ -13,3 +13,11 @@ func ClonePath(vcs string, cloneURL *url.URL) string {
 	numPathComponents := strings.Count(cloneURL.Path, "/")
 	return "/" + strconv.Itoa(numPathComponents) + "/" + vcs + "/" + cloneURL.Scheme + "/" + cloneURL.Host + cloneURL.Path
 }
+
+// FilePath returns the HTTP request path on vcsserver that maps to the
+// specified file at revision. Applications that use vcsserver to proxy
+// repositories should construct file URLs with the host URL of vcsserver and
+// the path returned by this function.
+func FilePath(vcs string, cloneURL *url.URL, revision, file string) string {
+	return ClonePath(vcs, cloneURL) + "/v/" + revision + "/" + file
+}
--- url_test.go
+++ url_test.go
@@ -27,3 +27,28 @@ func TestClonePath(t *testing.T) {
 		}
 	}
 }
+
+func TestFilePath(t *testing.T) {
+	tests := []struct {
+		vcs          string
+		cloneURL     string
+		revision     string
+		file         string
+		wantFilePath string
+	}{
+		{"git", "git://example.com/foo.git", "master", "foo.txt", "/1/git/git/example.com/foo.git/v/master/foo.txt"},
+		{"git", "https://example.com/foo/bar.git", "1234abcdef", "my/file.txt", "/2/git/https/example.com/foo/bar.git/v/1234abcdef/my/file.txt"},
+	}
+
+	for _, test := range tests {
+		cloneURL, err := url.Parse(test.cloneURL)
+		if err != nil {
+			t.Errorf("%s: url.Parse failed: %s", test.cloneURL, err)
+			continue
+		}
+		filePath := FilePath(test.vcs, cloneURL, test.revision, test.file)
+		if test.wantFilePath != filePath {
+			t.Errorf("%s %s: want filePath %s, got %s", test.cloneURL, test.file, test.wantFilePath, filePath)
+		}
+	}
+}
