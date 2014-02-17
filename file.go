6e9b9afca4292ec50c671e13b7ec5631f4758fcf
Yin Wang <yinwang0@gmail.com>
2014-02-17 13:46:29 -0800

    change string to FileType

--- file.go
+++ file.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
-func file(w http.ResponseWriter, r *http.Request, vcs vcs.VCS, dir string, extraPath string) *httpError {
+func file(w http.ResponseWriter, r *http.Request, vc vcs.VCS, dir string, extraPath string) *httpError {
 	extraPath = strings.TrimPrefix(extraPath, "/v/")
 	parts := strings.SplitN(extraPath, "/", 2)
 	if len(parts) != 2 {
 		return &httpError{"bad file path", http.StatusNotFound}
 	}
 	rev, path := parts[0], parts[1]
-	v, err := vcs.Open(dir)
+	v, err := vc.Open(dir)
 	if err != nil {
 		log.Print(err)
 		return &httpError{"failed to open repository", http.StatusInternalServerError}
@@ -28,7 +28,7 @@ func file(w http.ResponseWriter, r *http.Request, vcs vcs.VCS, dir string, extra
 		log.Print(err)
 		return &httpError{"failed to read file at revision", http.StatusInternalServerError}
 	}
-	if filetype == "Dir" {
+	if filetype == vcs.Dir {
 		w.Header().Set("Content-Type", "application/x-directory")
 	}
 	w.Write(data)
